ee60c20c46910d35f0098f8d4ae787193005b56e
Josh Burd <Jburdt@gmail.com>
2023-07-04 11:11:11 -0400

    fixed date test

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,9 +1,6 @@
 require "test_helper"
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
   test "should not save todo without task" do 
     todo = Todo.new
@@ -11,10 +8,8 @@ class TodoTest < ActiveSupport::TestCase
   end
 
   test "valid if the reminder is less than the todo" do 
-    def setup
-      @todo = Todo.new(task: "test2", due_on: Fri, 30 Jun 2024 10:41:00.000000000 UTC +00:00, remind_at: Fri, 31 Jun 2023 00:43:00.000000000 UTC +00:00)
-    end
-
-  end
+    @todo = Todo.new(task: "test2", due_on: Date.today, remind_at: 2.days.from_now)
+    @todo.valid?
+   end
 
 end
