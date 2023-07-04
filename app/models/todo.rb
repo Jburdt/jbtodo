3e73e745e4871400676aa5eeda90c8a76d654f8b
Josh Burd <Jburdt@gmail.com>
2023-07-04 12:09:17 -0400

    got all validation tests to work

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,6 @@
 class Todo < ApplicationRecord
-  enum status: { new: "new", complete: "complete" }
-  validates :status, inclusion: { in: status.keys }
+  enum status: { fresh: "new", complete: "complete" }
+  validates :status, inclusion: { in: statuses.keys }
   validates :task, presence: true
   validates :remind_at, comparison: { less_than: :due_on }
 end
--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -3,24 +3,25 @@ require "test_helper"
 class TodoTest < ActiveSupport::TestCase
 
   test "should not save todo without task" do 
-    todo = Todo.new
-    assert_not(todo.save, ["Todo needs can not be blank"])
+    @todo = Todo.new
+    assert_not(@todo.save, ["Todo needs can not be blank"])
   end
 
   test "valid if the reminder is less than the todo" do 
-    @todo = todos("invalid")
+    @todo = todos("invalid") # Grabbed the invalid data from fixtures
     refute @todo.valid?
     @todo.remind_at = @todo.due_on - 15.minutes 
     assert @todo.valid?
   end
 
   test "enum is set to new" do
-    @todo = todos("new_task")
+    @todo = todos("new_task") # Grabbed the new_task data from fixtures
     assert @todo.valid?
     @todo.status = "complete"
     assert @todo.valid?
-    @todo.status = "notvaild"
-    refute @todo.valid?
+    assert_raises ArgumentError do
+      @todo.status = "notvaild"
+    end
   end
 
 end
