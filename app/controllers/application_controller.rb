439eb893b1790e481aa585c5585de095b7f02184
Josh Burd <Jburdt@gmail.com>
2023-09-15 10:24:30 -0400

    added devise views for styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || todos_path
+    return_path = stored_location_for(resource)
+    return_path == root_path ? todos_path : return_path
   end
 end
