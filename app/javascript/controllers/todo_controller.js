9225814f74f007ee9836e86e8ce668fd6c8e5bcf
Glenn Goodrich <glenn.goodrich@method.com>
2023-11-09 15:55:52 +0100

    It's kind off working

--- app/javascript/controllers/todo_controller.js
+++ app/javascript/controllers/todo_controller.js
@@ -1,13 +1,15 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  targets=[];
+  static targets=["form"];
   connect() {
 	console.log("arse")
   }
 
   toggle(e) {
 	console.log("click ", e)
+	console.log(this.formTarget)
+	this.formTarget.submit();
   }
 }
 
