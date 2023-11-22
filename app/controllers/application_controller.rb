5975e3101ad592c562f022a325166a0cf6a44fb8
Glenn Goodrich <glenn.goodrich@method.com>
2023-11-22 16:22:17 +0100

    Clean up some code formatting issues
    Really just testing out LazyVim

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,28 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :get_quote
+  before_action :quote
 
   def after_sign_in_path_for(resource)
     return_path = stored_location_for(resource)
     return_path == root_path ? todos_path : return_path
   end
 
-  def get_quote
+  def quote
     cached = Kredis.json Date.today.to_s
-    puts cached.value
-    if !cached.value.nil?
+    unless cached.value.nil?
       j = JSON.parse(cached.value)
-      puts "Cached quote"
-      @quote = QuotesApi::Quote.new(j["quote"], j["author"])
+      @quote = QuotesApi::Quote.new(j['quote'], j['author'])
       return
     end
 
+    load_quote
+  end
+
+  private
+
+  def load_quote
     @quote = QuotesApi.new.get
     j = Kredis.json Date.today.to_s
     j.value = @quote.to_json
-    return @quote
   end
 end
